641b5cea05cb09f0dc1cc8b65eaaa1232250518c
Ola Eriksen <olae15@uia.no>
2016-01-21 19:48:31 +0100

    Da har jeg prøvd meg på noe jeg ikke er flink på

--- Python oppgaver/steinSaksPapir.py
+++ Python oppgaver/steinSaksPapir.py
@@ -2,7 +2,31 @@ import random
 
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-alternativer = ["Stein", "saks", "papir"]
-Stein = 1
-Saks = 2
-Papir = 3
+alternativer = [stein, saks, papir]
+
+spiller = raw_input ("stein, saks, eller papir")
+
+random.data
+
+
+while: 
+    if: spiller = "stein" and data = saks
+    print "Du vant"
+    elif spiller = "saks" and data = papir
+    print "Du vant"
+    elif spiller = "papir" and data stein
+    print "Du vant"
+    elif spiller = "stein" and data = papir
+    print "Du tapte"
+    elif spiller = "saks" and data = stein
+    print "Du tapte"
+    elif spiller = "papir" and data = saks
+    print "Du tapte"
+    elif spiller = "stein" == data = stein
+    print "Det ble uavgjort"
+    elif spiller = "saks" == date = saks
+    print "Det ble Uavgjort"
+    elif spiller = "papir" == data = papir
+    print "Det ble uavgjort"
+    
+    #Håper dette er noe som kan brukes videre folkens!! :D GLHF 
