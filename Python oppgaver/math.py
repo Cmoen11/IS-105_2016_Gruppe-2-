d5d746f0db2b356c06d888ec2047b8214fb47c60
Cmoen11 <cmoen11@gmail.com>
2016-01-21 17:36:27 +0100

    la til oppgaver

--- Python oppgaver/math.py
+++ Python oppgaver/math.py
@@ -1,2 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+tall1 = 1
+tall2 = 2
+
+# denne tar du Ola
+def pluss() :
+    
+def minus () :
+
+def gange () :
+
+#dele denne tar du Tommy WOA EA
+def dele () : 
+    
+
+def printResultat () : 
+    
+    
