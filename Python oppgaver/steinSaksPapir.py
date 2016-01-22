80a66c70c19c11550fd18d316f18b9cbc10f8c1c
Cmoen11 <cmoen11@gmail.com>
2016-01-22 19:11:39 +0100

    La til en ny version av stein saks papir

--- Python oppgaver/steinSaksPapir.py
+++ Python oppgaver/steinSaksPapir.py
@@ -1,126 +1,110 @@
-import random
-
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from random import randint
 
-spiller = raw_input ("stein,saks,papir:")
+alternativer = ["Stein","Saks","Papir"]
 
-data = random.random()
-
-if data <=0.33:
-    data = "stein"
-    
-elif data <=0.66:
-    data = "papir"
-    
-else:
-    data = "saks"
+def run() :
+    # Player chooses & Computer randomly choose
+    play()
     
 
-def spill(valgEn,valgTo):
-    if valgEn == valgTo:
-        print "Du valgte", valgEn
-        print "Dataen valgte", valgTo
-        print "Det ble likt"
+# Player chooses & Computer randomly choose
+def play() :
+    # player choose
+    inputChoose = raw_input("Vennligst velg mellom Stein(0), Saks(1) eller Papir(2) : ")
+       
+    # Parse the string over to int
+    try:
+        userChoosed = int(inputChoose)
+    except ValueError :
+        print "Vennligst fyll ut med tall."
+        play()
+    
+    if (0 > userChoosed) or (userChoosed > 2):  
+        print "Vennligst velg et tall mellom 0 - 2"
+        play()
         
-    elif valgEn == "stein":
-        if valgTo == "saks":
-            print "Du valgt", valgEn
-            print "Dataen valgte", valgTo
-            print "Du vant! Gratulerer"
+
         
-        else:
-            print "Du valgt", valgEn
-            print "Dataen valgte", valgTo
-            print "Du tapte, bedre lykke neste gang!"
-            
+    else :
+        # Computer random choose
+        gameChoosed = randint(0,2)    
     
-    elif valgEn == "papir":
-        if valgTo == "stein":
-            print "Du valgt", valgEn
-            print "Dataen valgte", valgTo
-            print "Du vant! Gratulerer!"
-                    
-        else:
-            print "Du valgt", valgEn
-            print "Dataen valgte", valgTo
-            print "Du tapte, bedre lykke neste gang!"    
+        # Print out game status
+        getStatus(userChoosed, gameChoosed)    
+    
+        # calculate who the winner is
+        calculateWinner(userChoosed, gameChoosed)
+    
+# get status
+def getStatus(userChoosed, gameChoosed) :
+    print "***************************************************"
+    print "Spiller kaster "  + alternativer[userChoosed] + " | Maskin kaster " + alternativer[gameChoosed]
+    print "***************************************************"
+    
+
+
+def calculateWinner(userChoosed, gameChoosed) :
+    
+    # 0 = Stein
+    # 1 = Saks
+    # 2 = Papir
+    
+    # Saks slÃ¥r papir
+    # Stein slÃ¥r Saks
+    # saks slÃ¥r papir
+    
+    playerWonText = "Spiller Vinner"
+    computerWonText = "Datamaskinen vinner"
+    tieText = "Det ble uavgjort"
+    
+    # alle mulige spill der spiller vinner
+    if (userChoosed is 0 and gameChoosed is 1) \
+       or (userChoosed is 1 and gameChoosed is 2) \
+       or (userChoosed is 2 and gameChoosed is 0) :
+        print playerWonText
+    
+    # alle mulige spill der datamaskinen vinner
+    elif (userChoosed is 1 and gameChoosed is 0) \
+         or (userChoosed is 2 and gameChoosed is 1) \
+         or (userChoosed is 0 and gameChoosed is 2) :
+        print computerWonText
+
+    # alle mulige spill der det kan bli uavgjort
+    elif (userChoosed is gameChoosed) :
+        print tieText
+    
+
+    
+    #print a break line
+    print "***************************************************"
+    
+    # Ask user if he wants to play again    
+    playAgain()    
+
+# ask the user if he wants to play again    
+def playAgain() :
+    # Give the user able to choose
+    answer = raw_input("Spill igjen, Y or N: ")
+    # Set the answer to uppercase
+    answer = answer.upper()
+    if (answer == "Y") or (answer == "N") :
         
-        
-    elif valgEn == "saks":
-        if valgTo == "papir":
-            print "Du valgt", valgEn
-            print "Dataen valgte", valgTo
-            print "Du vant! Gratulerer!"
-                    
-        else:
-            print "Du valgt", valgEn
-            print "Dataen valgte", valgTo
-            print "Du tapte, bedre lykke neste gang!"    
+        if (answer == "Y") : 
+            run()
+    else :
+        errorMessage(1)
     
-spill(spiller, data)
-
-    
-#Håper dette er noe som kan brukes videre folkens!! :D GLHF 
-
-# Jeg tok vekk det ola skreiv, og la inn en variant som regner ut ifra desimal 0-0.33 = Stein, 0.34-0.66 = saks og 0.67-1 = Papir. 
-# Nå funker programmet, kanskje ikke optimalt, da det papir har litt ekstra sjans for å bli trukket, men det jeg fikk til. Med litt hjelp fra internett selvfølgelig.
-
-# Dette er Tommy sin versjon. Var ikke sikker på om vi alle skulle jobbe på det sammen eller ikke.
-# Ville stått "import random" på toppen. 
-
-dataValg = random.randint (1,3)
-spillerVelger = True
-
-if dataValg == 1:
-    dataValg = "Stein"
-elif dataValg == 2:
-    dataValg = "Saks"
-elif dataValg == 3:
-    dataValg = "Papir"
-
-while spillerVelger == True:
-    print("Stein! Saks! Papir! Hva velger du?")
-    spillerValg input()
-    if spillerValg == "Stein":
-        print("Du har valgt stein!")
-        spillerValg = "Stein"
-        spillerVelger = False
-    elif spillerValg == "Saks":
-        print("Du har valgt saks!")
-        spillerValg= "Saks"
-        spillerVelger = False
-    elif spillerValg == "Papir":
-        print("Du har valgt papir!")
-        spillerValg = "Papir"
-        spillerVelger = False
-    else:
-        print("Det er ikke et valg!")
-        spillerVelger = True
-
-input()
-
-print ("Din motstander valgte " +dataValg+ "!")
-
-input()
-
-if spillerValg == "Stein" and dataValg == "Papir":
-    print("Du tapte!")
-elif spillerValg == "Saks" and dataValg == "Stein":
-    print("Du tapte!")
-elif spillerValg == "Papir" and dataValg == "Saks":
-    print("Du tapte!")
-elif spillerValg == "Stein" and dataValg == "Saks":
-    print("Du vant!")
-elif spillerValg == "Saks" and dataValg == "Papir":
-    print("Du vant!")
-elif spillerValg == "Papir" and dataValg == "Stein":
-    print("Du vant!")
-elif spillerValg == "Stein" and dataValg == "Stein":
-    print("Uavgjort!")
-elif spillerValg == "Saks" and dataValg == "Saks":
-    print("Uavgjort!")
-elif spillerValg == "Papir" and dataValg == "Papir":
-    print("Uavgjort!")
+# display error message
+def errorMessage(errorMessage):
     
     
+    # if user has entered inn wrong under "play again"
+    if (errorMessage is 1) :
+        print "Vennligst tast inn Y eller N"
+        playAgain()    
+
+
+# run the program
+run()
