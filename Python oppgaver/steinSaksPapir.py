ceca581e2d1ed383ddf8f546eae80352bdcfbcc0
Lonlontommy <Heisann852@hotmail.com>
2016-01-22 18:18:16 +0100

    Prøvde på en ny modell av stein saks papir, litt inspirasjon fra nettet

--- Python oppgaver/steinSaksPapir.py
+++ Python oppgaver/steinSaksPapir.py
@@ -65,3 +65,62 @@ spill(spiller, data)
 
 # Jeg tok vekk det ola skreiv, og la inn en variant som regner ut ifra desimal 0-0.33 = Stein, 0.34-0.66 = saks og 0.67-1 = Papir. 
 # Nå funker programmet, kanskje ikke optimalt, da det papir har litt ekstra sjans for å bli trukket, men det jeg fikk til. Med litt hjelp fra internett selvfølgelig.
+
+# Dette er Tommy sin versjon. Var ikke sikker på om vi alle skulle jobbe på det sammen eller ikke.
+# Ville stått "import random" på toppen. 
+
+dataValg = random.randint (1,3)
+spillerVelger = True
+
+if dataValg == 1:
+    dataValg = "Stein"
+elif dataValg == 2:
+    dataValg = "Saks"
+elif dataValg == 3:
+    dataValg = "Papir"
+
+while spillerVelger == True:
+    print("Stein! Saks! Papir! Hva velger du?")
+    spillerValg input()
+    if spillerValg == "Stein":
+        print("Du har valgt stein!")
+        spillerValg = "Stein"
+        spillerVelger = False
+    elif spillerValg == "Saks":
+        print("Du har valgt saks!")
+        spillerValg= "Saks"
+        spillerVelger = False
+    elif spillerValg == "Papir":
+        print("Du har valgt papir!")
+        spillerValg = "Papir"
+        spillerVelger = False
+    else:
+        print("Det er ikke et valg!")
+        spillerVelger = True
+
+input()
+
+print ("Din motstander valgte " +dataValg+ "!")
+
+input()
+
+if spillerValg == "Stein" and dataValg == "Papir":
+    print("Du tapte!")
+elif spillerValg == "Saks" and dataValg == "Stein":
+    print("Du tapte!")
+elif spillerValg == "Papir" and dataValg == "Saks":
+    print("Du tapte!")
+elif spillerValg == "Stein" and dataValg == "Saks":
+    print("Du vant!")
+elif spillerValg == "Saks" and dataValg == "Papir":
+    print("Du vant!")
+elif spillerValg == "Papir" and dataValg == "Stein":
+    print("Du vant!")
+elif spillerValg == "Stein" and dataValg == "Stein":
+    print("Uavgjort!")
+elif spillerValg == "Saks" and dataValg == "Saks":
+    print("Uavgjort!")
+elif spillerValg == "Papir" and dataValg == "Papir":
+    print("Uavgjort!")
+    
+    
