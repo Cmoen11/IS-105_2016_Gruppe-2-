ddde8255c7939d6a2b05bd354ae3fac1dd2ba02b
Cmoen11 <cmoen11@gmail.com>
2016-01-24 15:32:15 +0100

    Endret litt på egenoppgaven til christian moen

--- Python oppgaver/test_add_is105_Christian_Moen.py
+++ Python oppgaver/test_add_is105_Christian_Moen.py
@@ -1,5 +1,5 @@
 import unittest
-from math2 import add
+from math2_Christian_Moen import add
 
 class TestAdd(unittest.TestCase):
     def setUp(self):
@@ -8,6 +8,8 @@ class TestAdd(unittest.TestCase):
         self.assertEqual(add(5,2), 7)
     def test_numbers_2_2(self):
         self.assertEqual(add(2,2), 4)
+    def test_numbers_5_5(self):
+        self.assertEqual(add(5,5), 10)
 
 if __name__ == '__main__':
     unittest.main()
