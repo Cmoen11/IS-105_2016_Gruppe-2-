84c9829d08326647fcc1bb81116171e6fa5b005d
Cmoen11 <cmoen11@gmail.com>
2016-01-29 17:07:47 +0100

    la til poker

---
+++ uke 4/Poker.py
@@ -0,0 +1,45 @@
+'''
+   1.2.4
+
+   Poker!
+
+'''
+
+
+
+
+def giveCards() :
+    pass
+
+def calculateWinner() :     
+    pass
+
+def generateCards() :
+    pass
+
+
+class PokerCards:
+    
+    # Generated cards
+    counter = 0;
+    
+    def __init__ (symbole, value) :
+        '''
+        Symbole er da hvilken type kort det er. 
+        Value er da hvilken veri kortet har
+        
+        '''
+        self.symbole = symbole
+        self.value = value
+        
+        pokerCards.counter += 1
+            
+    def getValue(self) :
+        return self.value
+    
+    def getSymbole(self) :
+        return self.Symbole
+    
+    
+
+generateCards()
