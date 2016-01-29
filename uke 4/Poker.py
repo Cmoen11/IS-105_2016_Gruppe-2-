961fc1d40081a1309f016c18018e8e5b6e877f7e
Cmoen11 <cmoen11@gmail.com>
2016-01-29 18:18:39 +0100

    Laget ferdig generate Cards

--- uke 4/Poker.py
+++ uke 4/Poker.py
@@ -15,14 +15,54 @@ def calculateWinner() :
     pass
 
 def generateCards() :
-    pass
+    pokerCards = []
+    
+    ruter = 13
+    hjerter = 13
+    spar = 13
+    klover = 13
+    
+    
+    i = 0
+    x = 0
+    symbole = 1
+    value = 1
+    for x in range (0,4) :
+        
+        for i in range(0,13) :
+            pokerCards.append(PokerCards(symbole, value))
+            value += 1
+            
+        symbole += 1
+            
+    return pokerCards
+               
 
 
 class PokerCards:
+    '''
+    Symbole definasjon: 
+    1 = Ruter
+    2 = Hjerter
+    3 = Spar
+    4 = kløver
     
-    # Generated cards
-    counter = 0;
-    
+    Value definasjon
+    1.   2
+    2.   3
+    3.   4
+    4.   5
+    5.   6
+    6.   7
+    7.   8
+    8.   9
+    9.   10
+    10.  Knekt
+    11.  Dronning
+    12.  Konge
+    13.  Ess
+    '''
+
     def __init__ (symbole, value) :
         '''
         Symbole er da hvilken type kort det er. 
