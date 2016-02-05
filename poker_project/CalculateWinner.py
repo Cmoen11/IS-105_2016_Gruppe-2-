151eeb780e3243707b803435ea00ba0ffac9bd08
Cmoen11 <cmoen11@gmail.com>
2016-02-05 13:01:00 +0100

    lagt til en metode som regner ut vinneren

---
+++ poker_project/CalculateWinner.py
@@ -0,0 +1,37 @@
+
+
+def CalculateWinner(players) :
+  
+  winner1 = returnHighestObject(players[0], players[1]) 
+  winner2 = returnHighestObject(players[2], players[3])
+  winner3 = returnHighestObject(players[4], players[5]) 
+  
+  
+  if (winner1.getPoints() > winner2.getPoints()) \
+     and (winner1.getPoints() > winner3.getPoints()) :
+    return winner1
+  
+  if (winner2.getPoints() > winner1.getPoints()) \
+     and (winner2.getPoints() > winner3.getPoints()) :
+    return winner2
+  
+  if (winner3.getPoints() > winner1.getPoints()) \
+     and (winner3.getPoints() > winner1.getPoints()) :
+    return winner3  
+    
+    
+#This method take's tho objects, 
+#will return the object with highest value variable back
+
+
+
+def returnHighestObject (obj1, obj2):
+    
+    if obj1.getPoints() > obj2.getPoints():
+        return obj1
+    elif obj1.getPoints() == obj2.getPoints():
+        #Need to look for highest card...
+        return obj1
+    elif obj1.getPoints() < obj2.getPoints():
+      return obj2
+    
--- poker_project/Poker.py
+++ poker_project/Poker.py
@@ -2,19 +2,21 @@
 import random 
 from random import randint
 import Pointcalc_Poker
- 
+import CalculateWinner
 
 def deal(pokerCards) :
     '''
-    Create 5 player and deal out 5 cards to each player
+    Create 6 player and deal out 5 cards to each player
     '''
     players = []
     i = 0
     y = 0 # card number
-    for i in range(0,5) :
+    
+    # Create 6 players
+    for i in range(0,6) :
         cardToPlayer = []
         x = 0
-        
+        # Give each 6 player 5 cards
         for x in range(0,5):
             cardToPlayer.append(pokerCards[y])
             y += 1
@@ -133,3 +135,6 @@ for i in range(0,len(players)) :
     
     point = str(players[i].getPoints())
     print ("players scores " + point )
+    
+print "winner is:"
+print CalculateWinner.CalculateWinner(players).getName()
