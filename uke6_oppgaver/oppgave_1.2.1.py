e6b88d06a9b1dec9765f2c3f337acbf159478e8c
Cmoen11 <cmoen11@gmail.com>
2016-02-13 15:08:36 +0100

    lagt på kommentarer, og fikset litt opp i koden

--- uke6_oppgaver/oppgave_1.2.1.py
+++ uke6_oppgaver/oppgave_1.2.1.py
@@ -46,42 +46,43 @@ def encode(string, byte, table) :
 def run():
     sourcecode = "hamlet.txt"
     sourcecode2 = "output.txt"
-    f = open(sourcecode, 'r') # Open a file with filename <sourcecode>
-    kuk = open(sourcecode2, 'w') # Open a file with filename <sourcecode>
+    f = open(sourcecode, 'r') # Open a file that are beeing compressed.
+    outputFile = open(sourcecode2, 'w') # Open output file. 
     temp_holder = {}
-    table = code()
-    byte = ""
-    string = ""
-    #antallByte = 0
-    byte = f.read(1)
-    print len(byte)
-    global code_for_string
+    table = code() #add text-elements to our table.
+    byte = "" # String to hold our current byte.
+    string = "" # String to hold the last byte, for adding it later to the table
+    byte = f.read(1) # read the first byte.
     
-    #print encode(byte)
+    global code_for_string # Call the global code_for_string, so it can be edited.
     
-    
+    # First run of the first byte we read earlier.
     if (byte != "") :
+        # Encode the first byte
         temp_holder = encode(string, byte, table)
+        # add the return values to our local strings.
         string = temp_holder['string']
         table = temp_holder['table']
-        print table
+        
     # Kjør så lenge det er bytes i dokumentet
     while (byte != "") :
         # Hent ut en byte fra dokumentet
         byte = f.read(1)
         # Skriv til dokumentet, så lenge byte ikke er null
         if (byte != "") :
+            # Encode the byte
             temp_holder = encode(string, byte, table)
+            # Add the return values to the local verables.
             string = temp_holder['string']
             table = temp_holder['table']
     
     # legg til siste 'path' i code_for_string
     for k,v in table.iteritems():
         if v == string :
+            # Append key to our output table
             code_for_string.append(k)         
     
-    
-    print table
-    kuk.write(''.join(map(str,code_for_string)))    
+    # write code to output file, and strip it for extra chars, like (space,[]).
+    outputFile.write(''.join(map(str,code_for_string)))    
     
 run()
