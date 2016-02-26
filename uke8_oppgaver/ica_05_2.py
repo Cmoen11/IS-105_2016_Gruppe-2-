dd56028a9e18d37de2fb6b695c014e574fb7479b
Cmoen11 <cmoen11@gmail.com>
2016-02-26 16:39:37 +0100

    lagt til en endring, nå kan man måle tiden!

--- uke8_oppgaver/ica_05_2.py
+++ uke8_oppgaver/ica_05_2.py
@@ -1,27 +1,32 @@
 import timeit
-needle = 'test'
-fileName = "test.txt"
-haystack = open(fileName, 'r+').read()
+needle = 'T'
+fileName = "shakespare.txt"
+prepare = open(fileName, 'r').read()
 
 
-def search_fast(haystack, needle):
-    for item in haystack:
+def search_fast():
+    for item in prepare:
         if item == needle:
             return True
     return False
 
 
-def search_slow(haystack, needle):
+def search_slow():
     return_value = False
-    for item in haystack:
+    for item in prepare:
+        #print (item)
         if item == needle:
             return_value = True
     return return_value
 
 
 def runTest() :
-    print(timeit.timeit("search_slow(haystack, needle)", setup="from __main__ import search_slow, haystack, needle"))
-    print(timeit.timeit("search_fast(haystack, needle)", setup="from __main__ import search_fast, haystack, needle"))
+    n = 1000 # time how long n action's take
+    benchmark = timeit.Timer(search_slow)
+    print ("Search-slow :", benchmark.timeit(n), "seconds to go trough " + str(n) + " times")
+    
+    benchmark = timeit.Timer(search_fast)
+    print ("Search-fast :", benchmark.timeit(n), "seconds to go trough " + str(n) + " times")
 
 
 runTest()
