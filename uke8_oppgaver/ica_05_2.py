a5bdb41357b9e587dffb40bf9620fa65ebaeda48
Cmoen11 <cmoen11@gmail.com>
2016-02-26 18:30:46 +0100

    fikset litt kommentarer

--- uke8_oppgaver/ica_05_2.py
+++ uke8_oppgaver/ica_05_2.py
@@ -7,14 +7,14 @@ class eachLetter :
         self.fileName = "shakespare.txt"
         self.prepare = open(self.fileName, 'r').read()
     
-    # Go trough every letter, and look if they match, return True back when match is found
+    # Go trough every letter, and look for match, return True back when match is found
     def search_fast(self):
         for item in self.prepare:
             if item == self.needle:
                 return True
         return False
     
-    # Go trough every letter, and look if they match, set Return_value to true and return after running trough every letter
+    # Go trough every letter, look for match, set Return_value to true and return after running trough every letter
     def search_slow(self):
         return_value = False
         for item in self.prepare:
@@ -57,7 +57,7 @@ class eachWord :
     
     # Run test and print out time taken for running the tests
     def runTest(self) :
-        n = 10 # time how long n action's take
+        n = 1000 # time how long n action's take
         benchmark = timeit.Timer(self.search_slow)
         print ("Search-slow :", benchmark.timeit(n), "seconds to go trough " + str(n) + " times")
         
