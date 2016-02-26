034cb6367dc8cc59395c3e9f09443f162fa01fa4
Cmoen11 <cmoen11@gmail.com>
2016-02-26 16:03:35 +0100

    lagt enda en test

---
+++ uke8_oppgaver/ica_05_2.py
@@ -0,0 +1,27 @@
+import timeit
+needle = 'test'
+fileName = "test.txt"
+haystack = open(fileName, 'r+').read()
+
+
+def search_fast(haystack, needle):
+    for item in haystack:
+        if item == needle:
+            return True
+    return False
+
+
+def search_slow(haystack, needle):
+    return_value = False
+    for item in haystack:
+        if item == needle:
+            return_value = True
+    return return_value
+
+
+def runTest() :
+    print(timeit.timeit("search_slow(haystack, needle)", setup="from __main__ import search_slow, haystack, needle"))
+    print(timeit.timeit("search_fast(haystack, needle)", setup="from __main__ import search_fast, haystack, needle"))
+
+
+runTest()
