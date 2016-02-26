9567f838ccbae57a0fec375898fb103976e2d8a3
Bunnymann <Benjgb15@uia.no>
2016-02-26 14:44:58 +0100

    endret litt test i koden

--- uke8_oppgaver/Ica_05.py
+++ uke8_oppgaver/Ica_05.py
@@ -19,26 +19,29 @@ longsearch = """
 'hello' in text
 """
 
-splitsearch = """
+search_slow = """
 for line in text.splitlines():
     if 'hello' in line:
         break
 """
 
-presplitsearch = """
+search_fast = """
 for line in lines:
     if 'hello' in line:
         break
 """
 
+
+
+
 #Leter etter et definert ord i teksten og retunerer dette tiden pcen brukte på å finne dette ordet. 
 benchmark = timeit.Timer(longsearch, prepare)
 print "Finne første ord derfinert i kode:", benchmark.timeit(1000), "seconds"
 
-#
-benchmark = timeit.Timer(splitsearch, prepare)
-print "IN on splitted string takes:", benchmark.timeit(1000), "seconds"
+# Splitter hvert ord opp, hvis ordet er 'hello' så stopper den. 
+benchmark = timeit.Timer(search_slow, prepare)
+print "Search-slow :", benchmark.timeit(1000), "seconds"
 
-
-benchmark = timeit.Timer(presplitsearch, presplit_prepare)
-print "IN on pre-splitted string takes:", benchmark.timeit(1000), "seconds"
+# Splitter alt opp før den begynner å se etter ordet 'hello'. 
+benchmark = timeit.Timer(search_fast, presplit_prepare)
+print "Search-fast :", benchmark.timeit(1000), "seconds"
