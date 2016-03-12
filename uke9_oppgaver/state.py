12a7e73f2cd3aef0aaf550264991fae6bac77a26
Cmoen11 <cmoen11@gmail.com>
2016-03-12 11:41:55 +0100

    fikset litt kommentarer og struktur

--- uke9_oppgaver/state.py
+++ uke9_oppgaver/state.py
@@ -9,18 +9,21 @@ class State:
 
     def check_state(self):                                # if everything is left, as it would in the start of the game.
 
-        if 'left' in (self.tape.man, self.tape.chicken, self.tape.fox, self.tape.corn, self.tape.boat):
+        if self.check_lose_comboes():                     # check if the player has hit any death traps.
+            print("player is dead")                       # print art to the player NB: not created art for yet.
+            return None                                   # break the check
 
+        if 'left' in (self.tape.man, self.tape.chicken, self.tape.fox, self.tape.corn, self.tape.boat):
             print(self.art.art_a['a_with_items_person'])  # print the art for the position.
 
     def check_lose_comboes(self):
         '''
         This will check if the current state will lead to the player to lose
-        :return:
+        :return: True if the player is 'dead' | False if the user is not 'dead'
         '''
 
         # for ever condition where the man has left A wrong.
-        # where either chicken and corn is left alone, or fox or chicken is left alone.
+        # where either chicken and corn is left alone, or fox and chicken is left alone.
         if 'left' in (self.tape.boat, self.tape.chicken, self.tape.corn) and \
             'boat' in (self.tape.man, self.tape.fox) or \
                 'left' in (self.tape.boat, self.tape.chicken, self.tape.fox) and \
