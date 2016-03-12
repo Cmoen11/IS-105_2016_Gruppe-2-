50a1dbeaf8f56d15284416438a5aeaab05be1b23
Cmoen11 <cmoen11@gmail.com>
2016-03-12 11:42:50 +0100

    fikset stavefeil

--- uke9_oppgaver/state.py
+++ uke9_oppgaver/state.py
@@ -9,14 +9,14 @@ class State:
 
     def check_state(self):                                # if everything is left, as it would in the start of the game.
 
-        if self.check_lose_comboes():                     # check if the player has hit any death traps.
+        if self.check_lose_combo():                     # check if the player has hit any death traps.
             print("player is dead")                       # print art to the player NB: not created art for yet.
             return None                                   # break the check
 
         if 'left' in (self.tape.man, self.tape.chicken, self.tape.fox, self.tape.corn, self.tape.boat):
             print(self.art.art_a['a_with_items_person'])  # print the art for the position.
 
-    def check_lose_comboes(self):
+    def check_lose_combo(self):
         '''
         This will check if the current state will lead to the player to lose
         :return: True if the player is 'dead' | False if the user is not 'dead'
