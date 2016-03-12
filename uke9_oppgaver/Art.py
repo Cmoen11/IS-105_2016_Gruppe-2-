f5148deeb1abb42cb11ea2957bb24941eca5f499
Cmoen11 <cmoen11@gmail.com>
2016-03-12 11:13:57 +0100

    fikset litt på en mulig måte å illustrere art på

--- uke9_oppgaver/Art.py
+++ uke9_oppgaver/Art.py
@@ -2,18 +2,19 @@
 This includes all the arts for the boat project.
 '''
 
+class Art :
+    def __init__(self):
+        self.art_a = {       # Everything that include position A
+            'a_with_items_person': "     A \n           O\\\n          /|\\\n  ^^-@-^^^/-\+",
+            'a_with_items_noPerson': "     A\n\n  ^^-@-^^^---"
+        }
+        self.art_b = {       # Everything that include position A
 
-art_a = {       # Everything that include position A
-    'a_with_items_person': "     A \n           O\\\n          /|\\\n  ^^-@-^^^/-\+",
-    'a_with_items_noPerson': "     A\n\n  ^^-@-^^^---"
-}
-art_b = {       # Everything that include position A
+        }
 
-}
+        self.art_boat = {    # everything that includes boat
 
-art_boat = {    # everything that includes boat
-
-}
+        }
 
 print (art_a['a_with_items_noPerson'])
 print (art_a['a_with_items_person'])
--- uke9_oppgaver/state.py
+++ uke9_oppgaver/state.py
@@ -1,13 +1,15 @@
 import tape as t
+import art as art
 
 class State :
     def __init__(self):
-        self.tape = t.Database()                     # create new tape for the boat project
+        self.tape = t.Database()                            # create new tape for the boat project
+        self.art = art.Art()
 
     def check_state(self):
         # if everything is left, as it would in the start of the game.
         if 'left' in (self.tape.man, self.tape.chicken, self.tape.fox, self.tape.corn, self.tape.boat):
-            pass
+            print(art.Art.art_a['a_with_items_person'])     # print the art for the position.
 
     def check_lose_comboes(self):
         '''
@@ -15,4 +17,3 @@ class State :
         :return:
         '''
         pass
-    
