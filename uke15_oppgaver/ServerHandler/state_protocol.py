1ff558613456f293d9d66fa44e82f23a6ca9f2f8
Cmoen11 <cmoen11@gmail.com>
2016-05-27 23:09:55 +0200

    massiv fix på protokoll

--- uke15_oppgaver/ServerHandler/state_protocol.py
+++ uke15_oppgaver/ServerHandler/state_protocol.py
@@ -5,77 +5,39 @@ def state_protocol(tape, request):
     request_fragment = request.split()
     if request_fragment[0] == 'move':
         print request_fragment
+
+        # for moving the man, no need to check.
         if request_fragment[1] == 'man':
-            if request_fragment[2] == 'left':
-                if tape.boat == 'left' and tape.man == 'boat':
-                    return True
-                else:
-                    return False
-            elif request_fragment[2] == 'right':
-                if tape.boat == 'right' and tape.man == 'boat':
-                    return True
-                else:
-                    return False
-            elif request_fragment[2] == 'boat':
-                if (tape.man in 'left' and tape.boat in 'left') or (tape.man in 'right' and tape.boat in 'right'):
-                    return True
-                else:
-                    return False
+            return True
 
+        # for moving the chicken
         elif request_fragment[1] == 'chicken':
-            if request_fragment[2] == 'left':
-                if tape.boat == 'left' and tape.chicken == 'boat' and tape.man == 'left':
-                    return True
-                else:
-                    return False
-            elif request_fragment[2] == 'right':
-                if tape.boat == 'right' and tape.chicken == 'boat' and tape.man == 'right':
-                    return True
-                else:
-                    return False
-            elif request_fragment[2] == 'boat':
-                if (tape.boat in 'left' and tape.chicken in 'left' and tape.man in 'left') \
-                        or (tape.boat in 'right' and tape.chicken in 'right' and tape.man in 'right'):
-                    return True
-                else:
-                    return False
+            if request_fragment[2] in 'left' and tape.man in 'left' and tape.chicken in 'boat' or\
+                request_fragment[2] in 'right' and tape.man in 'right' and tape.chicken in 'boat' or\
+                 request_fragment[2] in 'boat' and tape.man in 'right' and tape.boat in 'right' and tape.chicken in 'right' or \
+                   request_fragment[2] in 'boat' and tape.man in 'left' and tape.boat in 'left' and tape.chicken in 'left' :
+                return True
+            else:
+                return False
 
+        # for moving the corn
         elif request_fragment[1] == 'corn':
-            if request_fragment[2] == 'left':
-                if tape.boat == 'left' and tape.corn == 'boat' and tape.man == 'left':
-                    return True
-                else:
-                    return False
-            elif request_fragment[2] == 'right':
-                if tape.boat == 'right' and tape.corn == 'boat' and tape.man == 'right':
-                    return True
-                else:
-                    return False
-            elif request_fragment[2] == 'boat':
-                if (tape.boat in 'left' and tape.corn in 'left' and tape.man in 'left') \
-                        or (tape.boat in 'right' and tape.corn in 'right' and tape.man in 'right'):
-                    return True
-                else:
-                    return False
+            if request_fragment[2] in 'left' and tape.man in 'left' and tape.corn in 'boat' or\
+                request_fragment[2] in 'right' and tape.man in 'right' and tape.corn in 'boat' or\
+                 request_fragment[2] in 'boat' and tape.man in 'right' and tape.boat in 'right' and tape.corn in 'right' or \
+                   request_fragment[2] in 'boat' and tape.man in 'left' and tape.boat in 'left' and tape.corn in 'left' :
+                return True
+            else:
+                return False
 
+        # for moving the fox
         elif request_fragment[1] == 'fox':
-            if request_fragment[2] == 'left':
-                if tape.boat == 'left' and tape.fox == 'boat' and tape.man == 'left':
-                    return True
-                else:
-                    return False
-            elif request_fragment[2] == 'right':
-                if tape.boat == 'right' and tape.fox == 'boat' and tape.man == 'right':
-                    return True
-                else:
-                    return False
-            elif request_fragment[2] == 'boat':
-                if (tape.boat in 'left' and tape.fox in 'left' and tape.man in 'left') \
-                        or (tape.boat in 'right' and tape.fox in 'right' and tape.man in 'right'):
-                    return True
-                else:
-                    return False
-            else :
+            if request_fragment[2] in 'left' and tape.man in 'left' and tape.fox in 'boat' or\
+                request_fragment[2] in 'right' and tape.man in 'right' and tape.fox in 'boat' or\
+                 request_fragment[2] in 'boat' and tape.man in 'right' and tape.boat in 'right' and tape.fox in 'right' or \
+                   request_fragment[2] in 'boat' and tape.man in 'left' and tape.boat in 'left' and tape.fox in 'left' :
+                return True
+            else:
                 return False
 
         elif request_fragment[1] == 'boat':
@@ -97,5 +59,5 @@ def state_protocol(tape, request):
 
 if __name__ == "__main__":
     print state_protocol(
-        Database(), 'move man left'
+        Database(), 'move fox right'
     )
